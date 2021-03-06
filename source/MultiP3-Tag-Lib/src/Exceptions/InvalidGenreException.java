f220af4f8aff7ba770ec4a7860a6db3984b9a2e4
Samuel Netherway <samuelnetherwaygit@gmail.com>
2021-03-06 17:21:51 +0000

    Added invalid genre exception.

---
+++ source/MultiP3-Tag-Lib/src/Exceptions/InvalidGenreException.java
@@ -0,0 +1,20 @@
+package Exceptions;
+
+//TODO - JavaDoc
+public class InvalidGenreException extends BaseException {
+
+    //TODO - JavaDoc
+    public InvalidGenreException() {
+        super();
+    }
+
+    //TODO - JavaDoc
+    public InvalidGenreException(String message) {
+        super(message);
+    }
+
+    //TODO - JavaDoc
+    public InvalidGenreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
