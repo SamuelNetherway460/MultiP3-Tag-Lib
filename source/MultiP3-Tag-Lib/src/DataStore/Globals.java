0283c48590d1ce2adaca35011417e133a2c1fa44
Samuel Netherway <samuelnetherwaygit@gmail.com>
2021-03-06 17:20:30 +0000

    Added Globals class for storage of global variables.

---
+++ source/MultiP3-Tag-Lib/src/DataStore/Globals.java
@@ -0,0 +1,11 @@
+package DataStore;
+
+/**
+ * A class which defines global variables accessible throughout the entire application.
+ */
+public class Globals {
+    // Application Details
+    public static final String APP_NAME = "MultiP3 Tag Lib";
+    public static final String APP_AUTHOR = "Samuel Netherway";
+    public static final String APP_VERSION = "1.0.0";
+}
